de5bd23e6d3115aff72ebf31c551f9a81e552916
jaewang-seo <jeawang.seo@gmail.com>
2020-10-14 17:37:53 +0900

    .

--- mysite/elections/views.py
+++ mysite/elections/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Candidate
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    # candidates = Candidate.objects.all()
+    # str = ''
+    # for candidate in candidates:
+    #     str += "<p>{} 기호{}번({})<br>".format(candidate.name, candidate.party_number, candidate.area)
+    #     str += candidate.introduction + "</p>"
+    # return HttpResponse(str)
+    candidates = Candidate.objects.all()
+    context = {'candidates' : candidates}
+    return render(request,'elections/index.html', context)
